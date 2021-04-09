c0fd8c63002053ec03ffd9ade87347ac2ed89adf
Rowan Lonsdale <hworblehat@fastmail.co.uk>
2021-04-09 11:38:28 +0100

    🐛 Reset System.out when complete
    Also convert indentation to tabs

--- GitHub/rotateSshKey/rotateSshKey_system.groovy
+++ GitHub/rotateSshKey/rotateSshKey_system.groovy
@@ -17,118 +17,124 @@ import org.apache.hc.core5.http.*
 import org.apache.hc.core5.http.io.HttpClientResponseHandler
 import org.apache.hc.core5.http.io.entity.HttpEntities
 
-System.out = out
-
 class JsonResponseHandler extends AbstractHttpClientResponseHandler<Object> {
-    private final JsonSlurper json = new JsonSlurper()
+	private final JsonSlurper json = new JsonSlurper()
 
-    @Override
-    Object handleEntity(HttpEntity entity) {
-        return entity.getContent().with { content ->
-            return json.parse(content)
-        }
-    }
+	@Override
+	Object handleEntity(HttpEntity entity) {
+		return entity.getContent().with { content ->
+			return json.parse(content)
+		}
+	}
 }
 
 class GitHub implements Closeable {
 
-    private static final String KEYS_URI = 'https://api.github.com/user/keys'
-    private final CloseableHttpClient httpClient = HttpClients.createDefault()
-    private final HttpClientResponseHandler<String> defaultResponseHandler = new BasicHttpClientResponseHandler()
-    private final HttpClientResponseHandler<Object> jsonResponseHandler = new JsonResponseHandler()
+	private static final String KEYS_URI = 'https://api.github.com/user/keys'
+	private final CloseableHttpClient httpClient = HttpClients.createDefault()
+	private final HttpClientResponseHandler<String> defaultResponseHandler = new BasicHttpClientResponseHandler()
+	private final HttpClientResponseHandler<Object> jsonResponseHandler = new JsonResponseHandler()
 
-    private final String authHeader
+	private final String authHeader
 
-    GitHub(String authToken){
-        this.authHeader = "token ${authToken}"
-    }
+	GitHub(String authToken){
+		this.authHeader = "token ${authToken}"
+	}
 
-    List<Map<String, ?>> getSshKeys() {
-        HttpGet req = new HttpGet(KEYS_URI)
-        addHeaders(req)
-        return httpClient.execute(req, jsonResponseHandler) as List<Map<String, ?>>
-    }
+	List<Map<String, ?>> getSshKeys() {
+		HttpGet req = new HttpGet(KEYS_URI)
+		addHeaders(req)
+		return httpClient.execute(req, jsonResponseHandler) as List<Map<String, ?>>
+	}
 
-    void addSshKey(String title, String publicKey) {
-        HttpPost req = new HttpPost(KEYS_URI)
-        addHeaders(req)
-        req.setEntity(HttpEntities.create("""\
-        {
-            "title": "${title}",
-            "key": "${publicKey.trim()}"
-        }
-        """.toString(), ContentType.APPLICATION_JSON))
-        httpClient.execute(req, defaultResponseHandler)
-    }
+	void addSshKey(String title, String publicKey) {
+		HttpPost req = new HttpPost(KEYS_URI)
+		addHeaders(req)
+		req.setEntity(HttpEntities.create("""\
+		{
+			"title": "${title}",
+			"key": "${publicKey.trim()}"
+		}
+		""".toString(), ContentType.APPLICATION_JSON))
+		httpClient.execute(req, defaultResponseHandler)
+	}
 
-    void deleteSshKey(int keyId) {
-        HttpDelete req = new HttpDelete("${KEYS_URI}/${keyId}")
-        addHeaders(req)
-        httpClient.execute(req, defaultResponseHandler)
-    }
+	void deleteSshKey(int keyId) {
+		HttpDelete req = new HttpDelete("${KEYS_URI}/${keyId}")
+		addHeaders(req)
+		httpClient.execute(req, defaultResponseHandler)
+	}
 
-    @Override
-    void close() {
-        httpClient.close()
-    }
+	@Override
+	void close() {
+		httpClient.close()
+	}
 
-    private void addHeaders(HttpUriRequest req) {
-        req.setHeader('Authorization', authHeader)
-        req.setHeader('Accept', 'application/vnd.github.v3+json')
-    }
+	private void addHeaders(HttpUriRequest req) {
+		req.setHeader('Authorization', authHeader)
+		req.setHeader('Accept', 'application/vnd.github.v3+json')
+	}
 
 }
 
-Map env = build.getEnvironment(listener)
-
-String publicKey
-String privateKey
-
-println "Generating new SSH key pair"
-JSch jsch = new JSch()
-KeyPair kpair = KeyPair.genKeyPair(jsch, KeyPair.RSA, 4096)
+def oldSysOut = System.out
+System.out = out
 try {
-    ByteArrayOutputStream privOs = new ByteArrayOutputStream()
-    kpair.writePrivateKey(privOs)
-    privateKey = new String(privOs.toByteArray(), 'UTF-8')
 
-    ByteArrayOutputStream pubOs = new ByteArrayOutputStream()
-    kpair.writePublicKey(pubOs, URI.create(env['JENKINS_URL']).host)
-    publicKey = new String(pubOs.toByteArray(), 'UTF-8')
+	Map env = build.getEnvironment(listener)
+
+	String publicKey
+	String privateKey
+
+	println "Generating new SSH key pair"
+	JSch jsch = new JSch()
+	KeyPair kpair = KeyPair.genKeyPair(jsch, KeyPair.RSA, 4096)
+	try {
+		ByteArrayOutputStream privOs = new ByteArrayOutputStream()
+		kpair.writePrivateKey(privOs)
+		privateKey = new String(privOs.toByteArray(), 'UTF-8')
+
+		ByteArrayOutputStream pubOs = new ByteArrayOutputStream()
+		kpair.writePublicKey(pubOs, URI.create(env['JENKINS_URL']).host)
+		publicKey = new String(pubOs.toByteArray(), 'UTF-8')
+	} finally {
+		kpair.dispose()
+	}
+
+	new GitHub(env['GITHUB_AUTH_TOKEN']).with { gitHub ->
+
+		String newKeyTitle = "${keyTitle}-${build.number}"
+		println "Adding new SSH public key to GitHub: ${newKeyTitle}"
+		gitHub.addSshKey(newKeyTitle, publicKey)
+
+		println "Saving private key to Jenkins' credentials: ${keyCredentialId}"
+		def newKey = new BasicSSHUserPrivateKey(
+			CredentialsScope.GLOBAL, keyCredentialId, 'git',
+			new BasicSSHUserPrivateKey.DirectEntryPrivateKeySource(privateKey),
+			'', keyCredentialDescription
+		)
+		def store = SystemCredentialsProvider.getInstance()
+		def oldKey = CredentialsProvider.findCredentialById(
+			keyCredentialId, SSHUserPrivateKey.class, build
+		)
+		if(oldKey) {
+			store.updateCredentials(Domain.global(), oldKey, newKey)
+		} else {
+			store.addCredentials(Domain.global(), newKey)
+		}
+
+		for(def key : gitHub.getSshKeys()) {
+			def m = key.title =~ /^${keyTitle}-(\d+)$/
+			if(m) {
+				int index = m.group(1).toInteger()
+				if(index < build.number-2 || index > build.number) {
+					println "Deleting old SSH public key from GitHub: ${key.title}"
+					gitHub.deleteSshKey(key.id)
+				}
+			}
+		}
+	}
+
 } finally {
-    kpair.dispose()
-}
-
-new GitHub(env['GITHUB_AUTH_TOKEN']).with { gitHub ->
-
-    String newKeyTitle = "${keyTitle}-${build.number}"
-    println "Adding new SSH public key to GitHub: ${newKeyTitle}"
-    gitHub.addSshKey(newKeyTitle, publicKey)
-
-    println "Saving private key to Jenkins' credentials: ${keyCredentialId}"
-    def newKey = new BasicSSHUserPrivateKey(
-        CredentialsScope.GLOBAL, keyCredentialId, 'git',
-        new BasicSSHUserPrivateKey.DirectEntryPrivateKeySource(privateKey),
-        '', keyCredentialDescription
-    )
-    def store = SystemCredentialsProvider.getInstance()
-    def oldKey = CredentialsProvider.findCredentialById(
-        keyCredentialId, SSHUserPrivateKey.class, build
-    )
-    if(oldKey) {
-        store.updateCredentials(Domain.global(), oldKey, newKey)
-    } else {
-        store.addCredentials(Domain.global(), newKey)
-    }
-
-    for(def key : gitHub.getSshKeys()) {
-        def m = key.title =~ /^${keyTitle}-(\d+)$/
-        if(m) {
-            int index = m.group(1).toInteger()
-            if(index < build.number-2 || index > build.number) {
-                println "Deleting old SSH public key from GitHub: ${key.title}"
-                gitHub.deleteSshKey(key.id)
-            }
-        }
-    }
+	System.out = oldSysOut
 }
